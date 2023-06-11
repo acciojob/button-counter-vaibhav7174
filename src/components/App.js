0be5e67e3eebd1ecb043d0c18949049a8e7d03d7
vaibhav7174 <vaibhavhumbade@gmail.com>
2023-06-11 13:22:17 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-
-import React ,{useState} from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [input,setInput] = useState(0);
-  const handleClick=()=>{
-    setInput(input+1);
-  }
+const [input, setInput] = useState(0)
+const handleClick = ()=>{
+  setInput(input+1);
+}
+
   return (
     <div>
-         <p>Button clicked {input} times</p>
-         <button onClick={handleClick}>Click me</button>
+        <p>Button clicked {input} times</p>
+        <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
